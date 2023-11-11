92d1eae86d6f1a713e2d42985bf9aa3fd96d93d4
Matti Ketonen <811436+kaardian@users.noreply.github.com>
2023-11-10 19:21:47 -0600

    Initial sercice set up. Will attach to the debugger if set to do so in the settings.

---
+++ TheService/Models/Logger.cs
@@ -0,0 +1,20 @@
+﻿using TheService.Properties;
+
+namespace TheService.Models
+{
+    /// <summary>
+    /// Logger class that sets up logging methods and values etc.
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// Defines the service name to log. Uses the Service name from settings.
+        /// </summary>
+        public static string LogSource { get { return Settings.Default.ServiceName; } }
+
+        /// <summary>
+        /// Defines the log location. Uses Windows Event Logs Application Log.
+        /// </summary>
+        public static string LogLocation { get { return "Application"; } }
+    }
+}
---
+++ TheService/OneService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TheService.Properties;
+using TheService.Models;
+
+namespace TheService
+{
+    public partial class OneService : ServiceBase
+    {
+        /// <summary>
+        /// Constructor, start of the applicaiton.
+        /// </summary>
+        public OneService()
+        {
+            InitializeComponent();
+            this.ServiceName = Settings.Default.ServiceName;
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = false;
+
+            // Initialize the Event Logs.
+            ((ISupportInitialize)this.EventLog).BeginInit();
+            if (!EventLog.SourceExists(this.ServiceName))
+                EventLog.CreateEventSource(this.ServiceName, Logger.LogLocation);
+
+            ((ISupportInitialize)this.EventLog).EndInit();
+
+            // Set the Event Log service and log name.
+            this.EventLog.Source = this.ServiceName;
+            this.EventLog.Log = Logger.LogLocation;
+        }
+
+        /// <summary>
+        /// Start service.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            this.EventLog.WriteEntry("Starting The One Service");
+            
+            // If debugging is enabled, and debugger is not connected, launch the debugger.
+            if (Settings.Default.LaunchDebugger &&
+                !Debugger.IsAttached)
+            {
+                this.EventLog.WriteEntry("Launching debugger");
+                System.Diagnostics.Debugger.Launch();
+            }
+        }
+
+        /// <summary>
+        /// Stop service.
+        /// </summary>
+        protected override void OnStop()
+        {
+            this.EventLog.WriteEntry("Stopping The One Service");
+        }
+    }
+}
